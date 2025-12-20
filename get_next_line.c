52ca3d0703406fe82d8f6c2859f3549e120f5900
Yassine El khader <yael-kha@c3r9p8.1337.ma>
2025-12-20 17:10:33 +0100

    get

--- get_next_line.c
+++ get_next_line.c
@@ -5,77 +5,110 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yael-kha <yael-kha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/10 18:25:00 by yael-kha          #+#    #+#             */
-/*   Updated: 2025/12/17 17:00:16 by yael-kha         ###   ########.fr       */
+/*   Created: 2025/12/20 01:41:17 by yael-kha          #+#    #+#             */
+/*   Updated: 2025/12/20 15:43:42 by yael-kha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 
-char	*_fill_line_buffer(int fd, char *left_c, char *buffer)
+char	*fill_line(int fd, char *what_left, char *buffer)
 {
-	int		bytes_read;
-	char	*temp;
+	char	*tmp;
+	int		was_read;
 
-	while (!ft_strchr(left_c, '\n'))
+	while (!ft_strchr(what_left, '\n'))
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read <= 0)
+		was_read = read(fd, buffer, BUFFER_SIZE);
+		if (was_read <= 0)
 			break ;
-		buffer[bytes_read] = '\0';
-		temp = ft_strjoin(left_c, buffer);
-		free(left_c);
-		left_c = temp;
-		if (!left_c)
+		buffer[was_read] = '\0';
+		tmp = ft_strjoin(what_left, buffer);
+		free(what_left);
+		what_left = tmp;
+		if (!what_left)
 			return (NULL);
 	}
-	return (left_c);
+	return (what_left);
 }
 
-char	*_set_line(char *line_buffer)
+char	*handling_fails(char **buffer)
+{
+	free(*buffer);
+	return (NULL);
+}
+
+char	*set_line(char *buffer_line, char **left_c)
 {
 	char	*line;
-	char	*remaining;
 	size_t	i;
 
 	i = 0;
-	while (line_buffer[i] && line_buffer[i] != '\n')
+	while (buffer_line[i] && buffer_line[i] != '\n')
 		i++;
-	if (line_buffer[i] == '\n')
+	if (buffer_line[i] == '\n')
 		i++;
-	line = ft_substr(line_buffer, 0, i);
-	remaining = ft_substr(line_buffer, i, ft_strlen(line_buffer) - i);
-	free(line_buffer);
-	if (remaining && remaining[0] == '\0')
+	line = ft_substr(buffer_line, 0, i);
+	if (!line)
+		return (handling_fails(&buffer_line));
+	*left_c = ft_substr(buffer_line, i, ft_strlen(buffer_line) - i);
+	free(buffer_line);
+	if (!*left_c)
+		return (handling_fails(&line));
+	if ((*left_c)[0] == '\0')
 	{
-		free(remaining);
-		remaining = NULL;
+		free(*left_c);
+		*left_c = NULL;
 	}
 	return (line);
 }
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
-	static char	*left_c;
-	char		buffer[BUFFER_SIZE + 1];
-	char		*line;
+	static char	*what_left;
+	char	*buffer;
+	char	*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!left_c)
-		left_c = ft_strdup("");
-	if (!left_c)
+	if (!what_left)
+		what_left = ft_strdup("");
+	if (!what_left)
 		return (NULL);
-	left_c = _fill_line_buffer(fd, left_c, buffer);
-	if (!left_c || left_c[0] == '\0')
+	buffer = malloc((size_t)BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	what_left = fill_line(fd, what_left, buffer);
+	free(buffer);
+	if (!what_left || what_left[0] == '\0')
 	{
-		free(left_c);
-		left_c = NULL;
+		free(what_left);
+		what_left = NULL;
 		return (NULL);
 	}
-	line = _set_line(left_c);
-	left_c = NULL;
+	line = set_line(what_left, &what_left);
 	return (line);
 }
+
+int main()
+{
+	// Example usage of get_next_line
+	int fd = open("example.txt", O_RDONLY | O_CREAT, 0644);
+	char *line;
+
+	if (fd == -1)
+		return (1);
+
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
+
--- get_next_line.h
+++ get_next_line.h
@@ -17,6 +17,9 @@
 #  define BUFFER_SIZE 42
 # endif
 
+# include <stdlib.h>
+# include <unistd.h>
+
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *s);
